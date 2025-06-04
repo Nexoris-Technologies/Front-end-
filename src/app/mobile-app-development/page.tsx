ef0ae66beb5ce3ea8e86d3e95844333d8d4b8206
Certsoftt <ukpeubong8573@gmail.com>
2025-06-04 14:10:24 +0100

    Update: the following pages with breadcrumb
    Added breadcrumb component to all the page app that are without one

--- src/app/mobile-app-development/page.tsx
+++ src/app/mobile-app-development/page.tsx
@@ -1,8 +1,17 @@
 import Serve from "@/components/Mobile-development-service/Servicepage";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import {StyledContainer} from "@/app/styles";
 
 export default function ServiceMobileApplication() {
+   const breadcrumbs = [
+    { name: "Home", href: "/", isCurrentPage: false },
+    { name: "Mobile Developement Service", href: "/mobile-app-development", isCurrentPage: true },
+  ];
   return (
     <main>
+      <StyledCntainer>
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+      </StyledContainer>
       <section>
         <div>
           <Serve />
