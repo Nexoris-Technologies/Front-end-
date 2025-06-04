d346ac43059f09a40cd9adfea6bb76f85e6ee64d
Certsoftt <ukpeubong8573@gmail.com>
2025-06-04 19:24:24 +0100

    Update: a minor type check

--- src/app/ClientComponent.tsx
+++ src/app/ClientComponent.tsx
@@ -10,7 +10,7 @@ export default function ClientComponent({
   // console.log(endpoint);
   useEffect(()=>{
     setEndpoint(window.location.pathname.split("/")[window.location.pathname.split("/").length - 1]);
-    const endpointArr = endpoint.split("-").map((part) => part.charAt(0).toUpperCase() + part.slice(1));
+    const endpointArr = endpoint.split("-").map((part:string) => part.charAt(0).toUpperCase() + part.slice(1));
     const title = endpointArr.join(" ");
     if(endpoint === ""){
       document.title = "Nexoris Technologies Ltd";
